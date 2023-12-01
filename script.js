e4f3d531d125943e3a4c07796c60d58dc6b411d2
Manikandan4080 <113871926+Manikandan4080@users.noreply.github.com>
2023-12-01 19:30:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age){
+		this._name = name;
+		this._age = age;
+	}
+	get name(){
+		return this._name;
+	}
+	set age(age){
+		this._age = age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study(){
+		console.log(`${this.name} is studying`);
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach(){
+		console.log(`${this.name} is teaching`);
+	}
+}
+
+const student1 = new Student("John", 20);
+student1.study();
+
+const teacher1 = new Teacher("Professor Smith", 35);
+teacher1.teach();
 
 // Do not change the code below this line
 window.Person = Person;
